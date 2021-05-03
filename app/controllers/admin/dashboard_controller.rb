f94218d47c3e4a17492a0415dc167ead00d20dbb
Dennis Wong <denniswong0106@gmail.com>
2021-05-03 06:50:37 +0000

    add passthrough data in dashboard_controller. Add basic display for product count and categories count in dashboard/show page.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def show
+    @product_count = Product.count
+    puts @product_count
+    @category_count = Category.count
+    puts @category_count
   end
 end
