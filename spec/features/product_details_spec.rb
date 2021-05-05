909be494be29a69acd2634c492c2c4d12ec45973
Dennis Wong <denniswong0106@gmail.com>
2021-05-05 09:55:50 +0000

    add basic structure into file

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+  before :each do
+  end
+
+  scenario "They see all products" do
+    # ACT
+
+
+    # DEBUG / VERIFY
+    save_screenshot
+
+  end
+
 end
