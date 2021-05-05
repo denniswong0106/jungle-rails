390a5854be9ac86c2ee33fb76c959ff3b0c6309c
Dennis Wong <denniswong0106@gmail.com>
2021-05-05 04:24:33 +0000

    add rspec tests for create new product - check if validation not met

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    context "given all fields are filled in, " do
+      it "saves successfully" do
+        @category = Category.new(:name => 'cars')
+        @product = Product.new(:name => 'Ferrari', :price => '1', :quantity => '1', :category => @category)
+        expect(@product.save).to be true
+      end
+    end
+
+    context ":name not filled in, " do
+      it "saves unsuccessfully" do
+        @category = Category.new(:name => 'cars')
+        @product = Product.new(:name => '', :price => '1', :quantity => '1', :category => @category)
+        expect(@product.save).to be false
+      end
+    end
+
+    context ":price not filled in, " do
+      it "saves unsuccessfully" do
+        @category = Category.new(:name => 'cars')
+        @product = Product.new(:name => 'Ferrari', :price => '', :quantity => '1', :category => @category)
+        expect(@product.save).to be false
+      end
+    end
+
+    context ":quantity not filled in, " do
+      it "saves unsuccessfully" do
+        @category = Category.new(:name => 'cars')
+        @product = Product.new(:name => 'Ferrari', :price => '1', :quantity => '', :category => @category)
+        expect(@product.save).to be false
+      end
+    end
+
+    context ":category not filled in, " do
+      it "saves unsuccessfully" do
+        @product = Product.new(:name => '', :price => '1', :quantity => '1')
+        expect(@product.save).to be false
+      end
+    end
+
+  end
+end
